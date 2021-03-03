6d20beb9a8cc70ed943ec01894396119d01f81ce
Ava Paulino <av590628@dal.ca>
2021-03-03 13:46:01 -0500

    php formatting

--- dist/csci1110.php
+++ dist/csci1110.php
@@ -28,9 +28,9 @@
                             </a>
                             <div class="sb-sidenav-menu-heading">Courses</div>
 
-                            <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                Home
+                            <a class="nav-link" href="csci1105.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                                CSCI 1105
                             </a>
 
                             <a class="nav-link" href="csci1107.php">
