22c3288c46b8866cfb6e5ac8d944342a0c49c2d5
Ava Paulino <av590628@dal.ca>
2021-03-03 14:31:50 -0500

    Instructions update

--- dist/csci1107.php
+++ dist/csci1107.php
@@ -145,6 +145,8 @@
                             <p>The sidebar on the left includes the CS courses which you can select to view a list of related LinkedIn Learning resources.</p>
                             <p>Below are tutorials on how to create an account and log in with the Halifax Public Libraries, and how to access LinkedIn Learning through Halifax Public Libraries. </p>
                             <p>NOTE: Students can register even if they are currently not in NS. When entering your address details, use the address of the faculty they are studying with or use the Dalhousie address. You can then select the box to declare that you are a Nova Scotia Resident.</p>
+							<p><strong>IMPORTANT: The library ID for LinkedInLearning is halifaxca</strong></p>
+                            <p><strong>IMPORTANT: The library ID for LinkedInLearning is halifaxca</strong></p>
                         </div>
                     </div>
                 </main>
---
+++ scripts/courses.php
@@ -0,0 +1,23 @@
+<?php
+  $query=$conn->prepare("SELECT CourseName, SuggestedCourse, Length, Note, Link, Picture FROM justintimeresources WHERE CourseNumber=?");
+  $query->bind_param("i", "CSCI 1105");
+  $query->execute();
+  $query->store_result();
+  if ($result->num_rows >0){
+    while($row=$result->fetch_assoc()){
+      $CourseName=$row["CourseName"];
+      $SuggestedCourse=$row["SuggestedCourse"];
+      $Length=$row["Length"];
+      $Note=$row["Note"];
+      $Link=$row["Link"];
+      $Picture=$row["Picture"];
+
+      echo "<div class='col-lg-3 col-md-6 col-sm-6 mb-2'>";
+      echo "<div class='card card-block'>";
+      echo "<img src=\'assets/".$Picture."\' alt=\'CSCI1105'>";
+      echo "<h5 class='card-title m1-2 mr-2 mt-3 mb-3'>".$CourseName."</h5>";
+      echo "<p class='card-text m1-2 mr-2'>".$Note."</p>";
+    }
+  }
+  $query->close();
+  ?>
