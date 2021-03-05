e1c29a96257f1d9183322079b7b73422120f7b20
Ava Paulino <av590628@dal.ca>
2021-03-05 14:50:16 -0500

    formatting php fixes

--- dist/scripts/connections.php
+++ dist/scripts/connections.php
@@ -31,12 +31,14 @@
             $Note=$row["Note"];
             $Link=$row["Link"];
             $Picture=$row["Picture"];
+            
             echo "\n\t\t\t\t\t\t<div class='col-lg-3 col-md-6 col-sm-6 mb-2'>\n";
-            echo "\t\t\t\t\t\t\t<div class='card card-block'>\n";
-            echo "\t\t\t\t\t\t\t\t<img src='assets/img/".$Picture."' alt='".$Picture."'>\n";
-            echo "\t\t\t\t\t\t\t\t<h5 class='card-title ml-2 mr-2 mt-3 mb-3'>".$CourseName."</h5>\n";
-            echo "\t\t\t\t\t\t\t\t<p class='card-text ml-2 mr-2'>".$Note."</p>\n";
-            echo "\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n";
+            echo "\t\t\t\t\t\t<a style='color: black; text-decoration: none;href='".$Link;
+            echo "\t\t\t\t\t\t\t\t<div class='card card-block'>\n";
+            echo "\t\t\t\t\t\t\t\t\t<img src='assets/img/".$Picture."' alt='".$Picture."'>\n";
+            echo "\t\t\t\t\t\t\t\t\t<h5 class='card-title ml-2 mr-2 mt-3 mb-3'>".$CourseName."</h5>\n";
+            echo "\t\t\t\t\t\t\t\t\t<p class='card-text ml-2 mr-2 scrollable'>".$Note."</p>\n";
+            echo "\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</a>\t\t\t\t\t\t</div>\n";
             }
         }
 
