be6616a8098ba3170b7043701bee99a667cf8cce
Nhat Nguyen <nhatnguyen@dal.ca>
2021-03-09 16:07:17 -0400

    Docker videos and docs

--- dist/docker.php
+++ dist/docker.php
@@ -151,7 +151,24 @@
                               </div>
                               <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                                 <div class="card-body">
+                                  <p>Here is the Docker Intro <a href=".\pdf\1. 00 Docker Introduction and Installation.pdf">documentation</a>.</p>
 
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=7379d797-5650-4d94-a5d3-ace50137e4a4&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=1c3fdd55-7e73-4985-a637-ace50137f41b&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=1e2cd512-03bf-4a41-a387-ace50137f8b1&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=76080364-4fcf-4a93-96ff-ace50137fc99&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=f7a62fdd-4df2-4348-bd91-ace501386bd4&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=b353f872-8e90-4834-b610-ace50138d00a&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=67ae49a9-90e1-4ed6-9f58-ace501399cdd&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=19ff5a40-6aff-43c7-92c7-ace5013a2202&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
                                 </div>
                               </div>
                             </div>
@@ -165,7 +182,64 @@
                               </div>
                               <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                                 <div class="card-body">
+                                      <p>Here is the <a href=".\pdf\3.%20Documentation_DockerFile.pdf">documentation</a>.</p>
+                                    <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=3d47225e-8aeb-43f0-b740-ace5013ddb6a&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                  </br>
+                                  <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=8ce30d34-b452-4b37-86ed-ace5013ddffa&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=0273b187-6af1-4d58-9500-ace5013de107&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                              </br>
+                              <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=a729ca2c-b0b1-4297-b4e0-ace5013de78c&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                            </br>
+                            <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=0190c37c-2c1c-4af6-bef2-ace5013deb1c&autoplay=false&offerviewer=true&showtitle=true&showbrand=true&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </div>
+                              </div>
+                            </div>
+                            <div class="card">
+                              <div class="card-header" id="headingThree">
+                                <h2 class="mb-0">
+                                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    Module 4
+                                  </button>
+                                </h2>
+                              </div>
+                              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                                <div class="card-body">
+                                  <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=7cf47c04-217b-4bd7-bf9e-ace5013fee5e&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                                </br>
+                                <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=3f9e41e9-3c16-4f9d-8909-ace5013ffcf5&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                              </br>
+                              <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=deb17417-99f3-4b00-a931-ace5013ffebe&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                            </br>
 
+                            <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=b25cad7d-7b5b-4096-b428-ace501400309&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+</br>
+<p><a href=".\pdf\4.1_Bind Mount_Documentation.pdf">Documentation</a> for Bind mount</p>
+
+                              <iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=bf53eb6d-5fd6-4b1c-b834-ace5014005bf&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+                            </br>
+                            <p><a href=".\pdf\4.2_Process_Documentation.pdf">Documentation</a> for Process</p>
+
+                                </div>
+                              </div>
+                            </div>
+                            <div class="card">
+                              <div class="card-header" id="headingFour">
+                                <h2 class="mb-0">
+                                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                    Module 5
+                                  </button>
+                                </h2>
+                              </div>
+                              <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
+                                <div class="card-body">
+<iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=ab71676c-387f-42c6-83db-ace501420684&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+</br>
+<iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=62587364-8ff9-47ca-aece-ace501420909&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+</br>
+<iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=2945dfaf-3589-4bea-8d1b-ace501420b73&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
+</br>
+<iframe src="https://dal.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=21580092-dcdd-43c9-8379-ace501420cd4&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" height="405" width="720" style="border: 1px solid #464646;" allowfullscreen allow="autoplay"></iframe>
                                 </div>
                               </div>
                             </div>
