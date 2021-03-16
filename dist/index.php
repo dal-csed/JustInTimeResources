28b81db2842972a9fdfe5538ff959ceeb8120ecf
Ava Paulino <av590628@dal.ca>
2021-03-16 13:51:07 -0400

    Added tab icons

--- dist/index.php
+++ dist/index.php
@@ -12,6 +12,8 @@
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
     <link rel="icon" type="image/svg+xml" href="./assets/img/house-door.svg">
+    <link rel="alternate icon" href="./assets/img/house-door.svg">
+    <link rel="mask-icon" href="./assets/img/house-door.svg" color="#ff8a01">
 </head>
 
 <body class="sb-nav-fixed">
