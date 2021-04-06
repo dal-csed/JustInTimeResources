02ad056055ee0bf0418a4aa21776df6d1539e283
Ava Paulino <av590628@dal.ca>
2021-04-06 11:13:40 -0400

    Course ID value fixes

--- dist/index.php
+++ dist/index.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <?php
-<<<<<<< HEAD
     include 'scripts/connections.php';
     $conn = connect();
     if (isset($_POST['submit'])){
@@ -12,18 +11,6 @@
 
         submitForm($conn, $dalCourse, $suggCourse, $comment);
     }
-=======
-$pageID = basename(__FILE__);
-include 'scripts/connections.php';
-$conn = connect();
-if (isset($_POST['submit'])) {
-    $dalCourse = $_POST["dalCourse"];
-    $suggCourse = $_POST["suggCourse"];
-    $comment = $_POST["comment"];
-
-    submitForm($conn, $dalCourse, $suggCourse, $comment);
-}
->>>>>>> like-dislike
 ?>
 
 <head>
@@ -176,14 +163,9 @@ if (isset($_POST['submit'])) {
                     <div class="row" style="padding-left: 2%;padding-right: 2%;">
                         <p>LinkedIn Learning, formally Lynda, is a website that offers different video courses taught by industry experts regarding software, creative, and business skills. It is a subsidiary of LinkedIn.</p>
                         <p>The sidebar on the left includes the CS courses which you can select to view a list of related LinkedIn Learning resources.</p>
-<<<<<<< HEAD
                         <p>Below are tutorials on how to create an account and log in with the Halifax Public Libraries, and how to access LinkedIn Learning through Halifax Public Libraries. </p>
                         <p>NOTE: Students can register even if they are currently not in NS. When entering your address details, use the address of the faculty they are studying with or use the Dalhousie address. You can then select the box to declare that you are a Nova Scotia Resident.</p>
                         <p><strong>IMPORTANT: The library ID for LinkedInLearning is halifaxca</strong></p>
-=======
-                        <p>Below are tutorials on how to create an account and log in with the Halifax Public Libraries, and how to access LinkedIn Learning through Halifax Public Libraries. </p>
-                        <p>NOTE: Students can register even if they are currently not in NS. When entering your address details, use the address of the faculty they are studying with or use the Dalhousie address. You can then select the box to declare that you are a Nova Scotia Resident.</p>
->>>>>>> like-dislike
                     </div>
                     <div class="accordion" id="accordionExample">
                         <div class="card">
